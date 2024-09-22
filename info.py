d2ee0f2b61cae4ea9270f7a84c0796d291729454
Aasifansa <aasifansari67543@gmail.com>
2024-09-22 19:20:21 +0530

    Update info.py

--- info.py
+++ info.py
@@ -89,10 +89,10 @@ APPROVED = environ.get("APPROVED_WELCOME", "on").lower()
 
 # Others
 VERIFY = bool(environ.get('VERIFY', False))
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'shareus.io')
-SHORTLINK_API = environ.get('SHORTLINK_API', '')
-SECOND_SHORTLINK_URL = environ.get('SECOND_SHORTLINK_URL', '')
-SECOND_SHORTLINK_API = environ.get('SECOND_SHORTLINK_API', '')
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'linkpays.in')
+SHORTLINK_API = environ.get('SHORTLINK_API', 'e27949c396504dca50e6caed52b70917f00593c9')
+SECOND_SHORTLINK_URL = environ.get('SECOND_SHORTLINK_URL', 'linkpays.in')
+SECOND_SHORTLINK_API = environ.get('SECOND_SHORTLINK_API', 'e27949c396504dca50e6caed52b70917f00593c9')
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 MAX_B_TN = environ.get("MAX_B_TN", "10")
